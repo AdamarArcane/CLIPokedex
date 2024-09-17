1f61002d5ae4a3f94a0bca2e51b8d0a2e53cc97d
adamarArcane <golywoper@gmail.com>
2024-09-17 17:10:15 -0400

    Added map and mapb functions for navigating locations

--- main.go
+++ main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
+
+	pokeapi "github.com/adamararcane/CLIPokedex/internal"
 )
 
+type Config struct {
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+}
+
+var config Config
+var commands map[string]cliCommand
+
 func main() {
+	if err := loadConfig(); err != nil {
+		fmt.Println("Error loading config:", err)
+		return
+	}
+
 	commands = map[string]cliCommand{
 		"help": {
 			name:        "help",
@@ -18,6 +34,16 @@ func main() {
 			name:        "exit",
 			description: "Exit the Pokedex",
 			callback:    commandExit,
+		},
+		"map": {
+			name:        "map",
+			description: "Displays next 20 locations",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays previous 20 locations",
+			callback:    commandMapb,
 		},
 	}
 
@@ -45,9 +71,11 @@ func main() {
 		} else {
 			fmt.Println("Invalid command! Type 'help' for available commands.")
 		}
-
 	}
 
+	if err := saveConfig(); err != nil {
+		fmt.Println("Warning: Could not save config.", err)
+	}
 }
 
 type cliCommand struct {
@@ -56,17 +84,109 @@ type cliCommand struct {
 	callback    func() error
 }
 
-var commands map[string]cliCommand
-
 func commandHelp() error {
 	fmt.Println("===== Commands =====")
-	fmt.Println("Usage:")
-	fmt.Printf("%s: %s\n", commands["help"].name, commands["help"].description)
-	fmt.Printf("%s: %s\n", commands["exit"].name, commands["exit"].description)
+	for _, value := range commands {
+		fmt.Printf("%s: %s\n", value.name, value.description)
+	}
 	return nil
 }
 
 func commandExit() error {
 	fmt.Println("Exiting the Pokedex")
+	return nil
+}
+
+func commandMap() error {
+	url := config.Next
+	if url == "" {
+		fmt.Println("No next page available.")
+		return nil
+	}
+
+	areas, nextUrl, previousUrl, err := pokeapi.GetPokeLocations(url)
+	if err != nil {
+		return err
+	}
+
+	// Update config
+	config.Next = nextUrl
+	config.Previous = previousUrl
+
+	if err := saveConfig(); err != nil {
+		fmt.Println("Warning: Could not save config.", err)
+	}
+
+	// Display the location area names
+	fmt.Println("Location Areas:")
+	for i, area := range areas {
+		fmt.Printf("%d. %s\n", i+1, area.Name)
+	}
+
+	return nil
+}
+
+func commandMapb() error {
+	url := config.Previous
+	if url == "" {
+		fmt.Println("No previous page available.")
+		return nil
+	}
+
+	areas, nextUrl, previousUrl, err := pokeapi.GetPokeLocations(url)
+	if err != nil {
+		return err
+	}
+
+	// Update config
+	config.Next = nextUrl
+	config.Previous = previousUrl
+
+	if err := saveConfig(); err != nil {
+		fmt.Println("Warning: Could not save config.", err)
+	}
+
+	// Display the location area names
+	fmt.Println("Location Areas:")
+	for i, area := range areas {
+		fmt.Printf("%d. %s\n", i+1, area.Name)
+	}
+
+	return nil
+}
+
+func saveConfig() error {
+	file, err := os.Create("config.json")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(config); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func loadConfig() error {
+	file, err := os.Open("config.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			// Initialize default config
+			config.Next = "https://pokeapi.co/api/v2/location-area/?limit=20"
+			config.Previous = ""
+			return nil
+		}
+		return err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		return err
+	}
+
 	return nil
 }
